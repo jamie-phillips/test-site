5eb8c4e20ee8e19dc8ba1597753cba6c52c873c5
jamie-phillips <jmpjamie@gmail.com>
2024-02-09 11:04:57 +0000

    Testing Code-Server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Wow what a great website 🤟");
+  res.send("Wow what a great website 🤟 - Test");
 });
 
 app.listen(port, () => {});
